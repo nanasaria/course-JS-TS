708d036b0ab2854c275515cd60b99e4270351315
Nayara Nasaria Soares da Silva <nnasaria@mundolivre.digital>
2025-03-20 20:16:20 -0300

    exercicio IMC - Lógica

---
+++ logica-programacao/Exercícios/imc/assets/js/main.js
@@ -0,0 +1,37 @@
+function private() {
+    const form = document.querySelector('form')
+    const resultado = document.querySelector('.resultado')
+    const imcHtml = document.querySelector('.imc')
+
+    //IMC = peso / (altura x altura)
+    function calcularIMC(evento) {
+        evento.preventDefault();
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+        const imc = (parseFloat(peso.value) / (Math.pow(parseFloat(altura.value), 2))).toFixed(2)
+
+        if (imc <= 18.5){
+            resultado.innerHTML = `Seu IMC é ${imc}`
+            imcHtml.innerHTML = `você está Abaixo do peso`
+        } else if (imc > 18.5 && imc <= 24.9) {
+            resultado.innerHTML = `Seu IMC é ${imc}`
+            imcHtml.innerHTML = `você está com Peso normal`
+        } else if (imc >= 25 && imc <= 29.9) {
+            resultado.innerHTML = `Seu IMC é ${imc}`
+            imcHtml.innerHTML = `você está com Sobrepeso`
+        } else if (imc >= 30 && imc <= 34.9) {
+            resultado.innerHTML = `Seu IMC é ${imc}`
+            imcHtml.innerHTML = ` você está com Obesidade grau 1`
+        } else if (imc >= 35 && imc <= 39.9) {
+            resultado.innerHTML = `Seu IMC é ${imc}`
+            imcHtml.innerHTML = ` você está com Obesidade grau 2`
+        } else if (imc >= 40) {
+            resultado.innerHTML = `Seu IMC é ${imc}`
+            imcHtml.innerHTML = ` você está com Obesidade grau 3`
+        }
+    }
+
+    form.addEventListener('submit', calcularIMC)
+}
+
+private()
---
+++ logica-programacao/string.js
@@ -0,0 +1,21 @@
+/*
+Operadores de comparação
+> - Maior que
+>= - Maior ou igual a
+< - Menor que
+<= - Menor que ou igual a
+== - Igualdade (valor)
+=== - Igualdade estrita (valor e tipo)
+!= - Diferente (valor)
+!== - Diferente estrito (valor e tipo)
+
+Operadores de Comparação
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+
+IF e ELSE 
+- Sempre que utilizar um else, irá ser necessário utilizar um if antes.
+- Pode ter diversos else if nas checagens
+- Somente um else na checagem
+*/
