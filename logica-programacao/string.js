64de7782e25ae007bd317bbbe48f8099891f9260
Nayara <nayaranasariasoares@gmail.com>
2025-03-20 23:03:01 -0300

    Objeto date

--- logica-programacao/string.js
+++ logica-programacao/string.js
@@ -18,4 +18,59 @@ IF e ELSE
 - Sempre que utilizar um else, irá ser necessário utilizar um if antes.
 - Pode ter diversos else if nas checagens
 - Somente um else na checagem
+
+Operador Ternário
+ex:
+*/
+const pontuacaoUsuario = 1000;
+const nivelUsuario =
+  pontuacaoUsuario >= 1000 ? "Usuário VIP" : "Usuário Normal";
+console.log(nivelUsuario);
+
+/*
+Objeto Date.
+
+Date é uma função construtora, então ela precisa ser instanciada.
+Em javascript, o mês começa em 0, então vai de 0 - 11.
+Podemos passar strings no objeto date.
+*/
+
+const tresHoras = 60 * 60 * 3 * 1000;
+const umDia = 60 * 60 * 24 * 1000;
+const data = new Date(0 + tresHoras + umDia);
+console.log(data.toString());
+
+const abril = new Date(2019, 3, 20, 15, 14, 27);
+console.log(abril);
+
+const dataString = new Date("2025-03-20 22:47:32.100");
+console.log("Dia", dataString.getDate());
+console.log("Mês", dataString.getMonth()); // Mês começa do 0
+console.log("Ano", dataString.getFullYear());
+console.log("Hora", dataString.getHours());
+console.log("Minuto", dataString.getMinutes());
+console.log("Segundo", dataString.getSeconds());
+console.log("Milésimo Segundo", dataString.getMilliseconds());
+console.log("Dia da Semana", dataString.getDay()); // Pega o número do dia da semana. 0 - Domingo, 6 - Sábado
+
+function addZero(num) {
+  return num >= 10 ? num : `0${num}`;
+}
+
+function formatDate(data) {
+  const dia = addZero(data.getDate());
+  const mes = addZero(data.getMonth() + 1);
+  const ano = addZero(data.getFullYear());
+  const hora = addZero(data.getHours());
+  const min = addZero(data.getMinutes());
+  const seg = addZero(data.getSeconds());
+
+  return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`;
+}
+
+const date = new Date();
+console.log(formatDate(date));
+
+/*
+Estrutura condicional Switch/Case
 */
