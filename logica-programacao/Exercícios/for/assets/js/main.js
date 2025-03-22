933de2cc44b1f657d651742a8e6eb31985b8ced1
Nayara <nayaranasariasoares@gmail.com>
2025-03-22 20:35:35 -0300

    Exercício For

--- logica-programacao/Exercícios/for/assets/js/main.js
+++ logica-programacao/Exercícios/for/assets/js/main.js
@@ -9,8 +9,33 @@ function private() {
   ];
 
   for (let i = 0; i < elementos.length; i++) {
-    container.innerHTML += `<${elementos[i].tag}> ${elementos[i].texto} </${elementos[i].tag}> <br>`;
+    const { tag, texto } = elementos[i];
+    container.innerHTML += `<${tag}> ${texto} </${tag}> <br>`;
   }
 }
 
 private();
+
+/*
+Outra forma de resolver o exercício - Forma do professor
+
+const elementos = [
+    { tag: "p", texto: "Frase 1" },
+    { tag: "div", texto: "Frase 2" },
+    { tag: "footer", texto: "Frase 3" },
+    { tag: "section", texto: "Frase 4" },
+  ];
+
+  const container = document.querySelector(".container");
+  const div = document.createElement("div");
+
+  for(let i = 0; i < elementos.length; i++) {
+   const {tag, texto} = elementos[i]
+   let elemento = document.createElement(tag)
+   elemento.innerHTML = texto;
+   div.appendChild(elemento)
+  }
+
+  container.appendChild(div)
+
+*/
