be86963563f469a113ad1d778675a8bed5e85493
Nayara <nayaranasariasoares@gmail.com>
2025-03-23 00:37:16 -0300

    ex 2 - logica

--- logica-programacao/Exercícios/logica-1/exercicio1.js
+++ logica-programacao/Exercícios/logica-1/exercicio1.js
@@ -8,3 +8,4 @@ console.log(maiorNumero(19, 35));
 
 // Outra forma com apenas 1 linha
 const max = (num1, num2) => (num1 > num2 ? num1 : num2);
+console.log(max(1945, 1935));
---
+++ logica-programacao/Exercícios/logica-2/exercicio2.js
@@ -0,0 +1,17 @@
+/*
+Escreva uma função chamada ePaisagem que recebe dois argumentos,
+largura e altura de uma imagem (number).
+Retorne true se a imagem estiver no modo paisagem.
+*/
+
+const ePaisagem = (altura, largura) => {
+  if (isNaN(altura)) {
+    return "altura inválida";
+  } else if (isNaN(largura)) {
+    return "largura inválida";
+  }
+
+  return Number(largura) > Number(altura) ? true : false;
+};
+
+console.log(ePaisagem(200, 250));
