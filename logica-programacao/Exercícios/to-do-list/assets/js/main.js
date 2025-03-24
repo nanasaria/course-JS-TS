fd7ee13071a71f053a6f2dae5d3839656e8fecb1
Nayara <nayaranasariasoares@gmail.com>
2025-03-23 23:54:21 -0300

    exercicio to do list

--- logica-programacao/Exercícios/to-do-list/assets/js/main.js
+++ logica-programacao/Exercícios/to-do-list/assets/js/main.js
@@ -2,27 +2,28 @@ const entrada = document.querySelector(".entrada");
 const tarefasHtml = document.querySelector(".tarefas");
 const addTarefa = document.querySelector(".addTarefa");
 
-// const ul = document.createElement("ul");
-// tarefasHtml.appendChild(ul);
+private = () => {
+  addTarefa.addEventListener("click", (e) => {
+    createTarefa();
+    entrada.value = "";
+  });
 
-addTarefa.addEventListener("click", (e) => {
-  createTarefa();
-  entrada.value = "";
-});
+  function createTarefa() {
+    const li = document.createElement("li");
+    const buttonDelete = document.createElement("button");
 
-function createTarefa() {
-  const li = document.createElement("li");
-  const buttonDelete = document.createElement("button");
-
-  buttonDelete.className = "excluirTarefa";
-  li.innerHTML = entrada.value;
-  tarefasHtml.appendChild(li);
-  li.appendChild(buttonDelete);
-  buttonDelete.innerText = "Excluir";
-}
-
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("excluirTarefa")) {
-    e.target.closest("li").remove();
+    buttonDelete.className = "excluirTarefa";
+    li.innerHTML = entrada.value;
+    tarefasHtml.appendChild(li);
+    li.appendChild(buttonDelete);
+    buttonDelete.innerText = "Excluir";
   }
-});
+
+  document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("excluirTarefa")) {
+      e.target.closest("li").remove();
+    }
+  });
+};
+
+private();
