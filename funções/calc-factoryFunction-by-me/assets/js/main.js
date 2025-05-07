c93171c5e14dd07094c7cb3be36c9a6b352b8303
Nayara Nasaria Soares da Silva <nnasaria@mundolivre.digital>
2025-05-06 21:32:32 -0300

    Estudando factory function

--- funções/calc-factoryFunction-by-me/assets/js/main.js
+++ funções/calc-factoryFunction-by-me/assets/js/main.js
@@ -1,41 +1,81 @@
-/*
-Calculadora com factoryFunction
-
-Contas normais
-
-se tiver ** vira uma potência
-
-parênteses executa a conta primeiro
-
-Se não for possível fazer a conta, 
-retorna erro em um alert: Conta inválida
-
-O que pensei:
-Pegar value dos botões depois de clicado.
-Pegar input.
-Executar apenas depois de apertar 
-no botão de =.
-*/
-
 const table = document.querySelector("table");
+const display = document.querySelector(".display");
 
-const values = [];
+let values = [];
+let resultOperation;
+
+function divideValues() {
+  let firstValue;
+  let secondValue;
+
+  const operators = ["+", "-", "*", "/"];
+
+  const indexOperator = values.findIndex((value) => operators.includes(value));
+
+  firstValue = values.slice(0, indexOperator).join("").toString();
+  secondValue = values
+    .slice(indexOperator + 1)
+    .join("")
+    .toString();
+
+  return { firstValue, secondValue };
+}
+
+function calculadora() {
+  const { firstValue, secondValue } = divideValues();
+
+  return {
+    firstValue,
+    secondValue,
+    calculate: function () {
+      if (values.includes("+")) {
+        return Number(this.firstValue) + Number(this.secondValue);
+      }
+      if (values.includes("-")) {
+        return Number(this.firstValue) - Number(this.secondValue);
+      }
+      if (values.includes("**")) {
+        return Number(this.firstValue) ** Number(this.secondValue);
+      }
+      if (values.includes("*")) {
+        return Number(this.firstValue) * Number(this.secondValue);
+      }
+      if (values.includes("/")) {
+        return Number(this.firstValue) / Number(this.secondValue);
+      }
+    },
+    deleteOneValue: function () {
+      const value = display.value;
+      values.pop();
+      const newValue = value.slice(0, -1);
+      display.value = newValue;
+    },
+    resetValues: function () {
+      display.value = "";
+      values = [];
+    },
+  };
+}
 
 table.addEventListener("click", (e) => {
   const tagElement = e.target.tagName;
   const valueElement = e.target.textContent;
 
+  const calc = calculadora();
+
   if (tagElement === "BUTTON") {
     if (valueElement === "C") {
-      console.log("C");
+      calc.resetValues();
     } else if (valueElement === "«") {
-      console.log("Delete");
+      calc.deleteOneValue();
+    } else if (valueElement === "=") {
+      resultOperation = calc.calculate();
+      display.value = resultOperation;
     } else {
+      if (valueElement !== undefined) {
+        display.value += valueElement;
+      }
       values.push(valueElement);
     }
   }
-
-  console.log(values);
 });
-
-function calculadora() {}
