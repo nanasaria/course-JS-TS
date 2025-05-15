d50cebbe0c0f8b5bfaf6c06428dc902d77e9f769
Nayara Nasaria Soares da Silva <nnasaria@mundolivre.digital>
2025-05-14 22:52:03 -0300

    Validate cpf by professor

---
+++ JavaScript/exercicios/arrays/main.js
@@ -0,0 +1,65 @@
+/*
+Map
+
+Dobrar os números
+Dado o array [1, 2, 3, 4], crie um novo array com cada número dobrado.
+
+Converter para string
+Dado o array [10, 20, 30], transforme todos os números em strings.
+
+Extrair nomes
+Dado o array:
+*/
+const users = [
+  { id: 1, name: "Ana" },
+  { id: 2, name: "Bruno" },
+  { id: 3, name: "Carlos" },
+];
+
+/*
+Filter
+
+Números maiores que 10
+Dado o array [5, 8, 13, 20, 3, 50], crie um novo array só com os números maiores que 10.
+
+Filtrar usuários ativos
+Dado:
+
+Filtrar nomes com mais de 5 letras
+Dado o array ['Ana', 'Bruno', 'Carlos', 'Eva'], filtre os nomes que têm mais de 5 letras.
+*/
+const users2 = [
+  { id: 1, name: "Ana", active: true },
+  { id: 2, name: "Bruno", active: false },
+  { id: 3, name: "Carlos", active: true },
+];
+
+/*
+Somar todos os números
+Dado o array [1, 2, 3, 4], use reduce para somar todos os números.
+
+Contar quantos números pares existem
+Dado o array [1, 2, 3, 4, 5, 6], use reduce para contar quantos números pares ele possui.
+
+Transformar array de strings em frase
+Dado ['Olá', 'mundo', 'Javascript'], use reduce para juntar tudo em uma única string com espaços.
+*/
+
+/*
+Dado o array:
+
+const products = [
+  { name: 'Mouse', price: 50 },
+  { name: 'Teclado', price: 80 },
+  { name: 'Monitor', price: 500 },
+  { name: 'Cabo HDMI', price: 30 },
+];
+
+Faça:
+
+Um array com os nomes dos produtos.
+
+Um array só com produtos que custam mais de 100.
+
+O valor total de todos os produtos.
+*/
