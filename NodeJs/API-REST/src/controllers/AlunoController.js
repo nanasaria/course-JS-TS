503d99cf333033729fa6177dd4b3292a9ec737bc
Nayara N Soares <nnasaria@mundolivre.digital>
2025-06-06 14:05:20 -0300

    Construindo API REST

---
+++ NodeJs/API-REST/src/controllers/AlunoController.js
@@ -0,0 +1,103 @@
+import Aluno from "../models/Aluno";
+
+class AlunoController {
+  async index(req, res) {
+    const alunos = await Aluno.findAll();
+    res.json(alunos);
+  }
+
+  async store(req, res) {
+    try {
+      const aluno = Aluno.create(req.body);
+      return res.json(aluno);
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ["Faltando o ID"],
+        });
+      }
+
+      const aluno = await aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Aluno não existe"],
+        });
+      }
+
+      return res.json(aluno);
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ["Faltando o ID"],
+        });
+      }
+
+      const aluno = await aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Aluno não existe"],
+        });
+      }
+
+      await aluno.destroy();
+      return res.json({
+        apagado: true,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ["Faltando o ID"],
+        });
+      }
+
+      const aluno = await aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Aluno não existe"],
+        });
+      }
+
+      const alunoAtualizado = await aluno.update(req.body);
+      return res.json(alunoAtualizado);
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((e) => e.message),
+      });
+    }
+  }
+}
+
+export default new AlunoController();
---
+++ NodeJs/API-REST/src/routes/alunoRoutes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import AlunoController from "../controllers/AlunoController";
+
+import loginRequired from "../middlewares/loginRequired";
+
+const router = new Router();
+
+/* Funções vão dentro do controller */
+router.get("/", AlunoController.index);
+router.post("/", loginRequired, AlunoController.store);
+router.put("/:id", loginRequired, AlunoController.update);
+router.get("/:id", AlunoController.show);
+router.delete("/:id", loginRequired, AlunoController.delete);
+
+export default router;
