1a912605e8add7e4b6b29be735438321aaf4b42f
Nayara Nasaria Soares <nayaranasariasoares@gmail.com>
2025-08-24 23:52:01 -0300

    feat: Começando os estudos de typescript

---
+++ TypeScript/annotations/notes.js
@@ -0,0 +1,35 @@
+/**
+O que é TypeScript?
+É um superset para o JavaScript com tipagem estática, novos recursos e que compila
+para JavaScript puro.
+
+TypeScript permite:
+
+tipagem estática -> define o tipo dos valores que passamos para funções ou
+variáveis.
+
+Interface -> Algo como um contrato, se a classe implementar, precisa 
+devolver o que está nas interfaces.
+
+Modificadores de acesso -> Podemos dizer que um campo é público, protegido, 
+privado...
+
+readonly -> Atributos configurados apenas no construtor e imutáveis.
+
+Classe abstrata -> Classe que serve como base para outras classes que herdam suas
+características e devem implementar seus métodos abstratos. Uma classe abstrata
+pode conter tanto métodos abstratos quanto métodos concretos (sem implementação).
+
+Método abstrato -> Métodos sem implementação.
+
+Type -> Espécie de contrato que define a estrutura e os valores que uma variável
+pode ter.
+
+Enum -> É um tipo de dado especial que permite definir um conjunto de constantes 
+nomeadas, tornando o código mais legível, organizado e seguro ao agrupar valores
+relacionados.
+
+TypeScript é multiparadigma. Ou seja, suporta mais de um paradigma de programação.
+TypeScript modela os tipos com inferência de tipos.
+
+*/
